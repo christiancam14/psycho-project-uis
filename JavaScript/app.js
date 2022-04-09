262b3a59b2d010872a0094707151c148e55a8ed4
Christian Camacho <chriscamacho1045@gmail.com>
2022-04-09 12:48:01 -0500

    Comienzo del proyecto psyco-project con Angular, y nodeJs

--- JavaScript/app.js
+++
@@ -1,9 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-    const btn_menu = document.querySelector('.btn_menu')
-    if(btn_menu) {
-        btn_menu.addEventListener('click', () => {
-            const main_item = document.querySelector('.main-nav')
-            main_item.classList.toggle('show')
-        })
-    }
-})
