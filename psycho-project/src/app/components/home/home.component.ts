fd940aadd406b3f4785e3a8c1dc3bb3a2a19db33
Christian Camacho <chriscamacho1045@gmail.com>
2022-04-19 12:50:49 -0500

    Adelanto del home

--- psycho-project/src/app/components/home/home.component.ts
+++ psycho-project/src/app/components/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public landingPageImg:any = "../../../assets/img/landing.jpg";
+
   constructor() { }
 
   ngOnInit(): void {
