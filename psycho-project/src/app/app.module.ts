95af9404be9f5432484bb1d1aa6d8b245c0c9c2b
Christian Camacho <chriscamacho1045@gmail.com>
2022-04-20 00:03:01 -0500

    Bootstrap

--- psycho-project/src/app/app.module.ts
+++ psycho-project/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { SingupComponent } from './components/singup/singup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
