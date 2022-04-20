9a4fe9d086cb166469991455c1ba203a0cb314f2
SrConn0r <64154806+SrConn0r@users.noreply.github.com>
2022-04-20 11:09:17 -0500

    Cambio blog y faq a pages Creacion footer

--- psycho-project/src/app/website/website-routing.module.ts
+++ psycho-project/src/app/website/website-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServicesComponent } from './pages/services/services.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { FaqComponent } from './components/faq/faq.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { FaqComponent } from './pages/faq/faq.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SingupComponent } from './pages/singup/singup.component';
--- psycho-project/src/app/website/website.module.ts
+++ psycho-project/src/app/website/website.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { ServicesComponent } from './pages/services/services.component';
-import { FaqComponent } from './components/faq/faq.component';
-import { BlogComponent } from './components/blog/blog.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { BlogComponent } from './pages/blog/blog.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SingupComponent } from './pages/singup/singup.component';
 import { HeaderComponent } from './components/header/header.component';
 
 import { WebsiteRoutingModule } from './website-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { WebsiteRoutingModule } from './website-routing.module';
     BlogComponent,
     LoginComponent,
     SingupComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
