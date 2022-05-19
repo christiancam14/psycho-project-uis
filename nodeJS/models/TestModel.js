918bd240b9a03458d674d08b5f7d443ad6675397
Juan Pablo Viviescas <juanpabloviviescas117@hotmail.com>
2022-05-19 06:33:45 -0500

    Pruebas de relacion muchos a muchos

--- nodeJS/models/TestModel.js
+++ nodeJS/models/TestModel.js
@@ -1,12 +1,19 @@
 // importamos conecion base de datos
 import db from "../database/db.js"
+/* import consultation from "./ConsultationModel.js" */
 // importamos sequelize
-import { DataTypes } from "sequelize"
+import { DataTypes, Sequelize } from "sequelize"
 
 const TestModel = db.define('tests', {
     report   : { type: DataTypes.STRING },
     createdAt: { type: DataTypes.DATE },
     updatedAt: { type: DataTypes.DATE }
 })
+
+//llaves foraneas
+const test = Sequelize.define('tests', {id_test : DataTypes.INTEGER})
+const consultation = Sequelize.define('consultations', {id_consultation:DataTypes.INTEGER})
+test.belongsToMany(consultation, { through: 'TestConsultation' })
+consultation.belongsToMany(test, { through: 'TestConsultation' })
 
 export default TestModel
